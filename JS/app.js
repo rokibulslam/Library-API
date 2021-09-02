733bcc79f129015024f2e8f71924ed3e4ea5103b
rokibulislamm <md>
2021-09-02 19:20:53 +0600

    class name edited

--- JS/app.js
+++ JS/app.js
@@ -2,7 +2,7 @@
 const inputText = document.getElementById('input-value');
 const spinner = document.getElementById('spinner')
 const errorDiv = document.getElementById('error')
-const itemFound = document.getElementById('not-found');
+const totalItem = document.getElementById('not-found');
 const parentDiv = document.getElementById('search-result');
 
 // get input value and fetch data
@@ -12,7 +12,7 @@ const searchField = () => {
     // Input Error Handling
     if (inptuValue === "") {
         parentDiv.textContent = '';
-        itemFound.innerHTML = '';
+        totalItem.innerHTML = '';
         errorDiv.innerText = "Search field Cannot be empty."
         spinner.classList.add("d-none")
         return;
@@ -20,7 +20,7 @@ const searchField = () => {
     // Clear 
     errorDiv.innerText = "";
     inputText.value = "";
-    itemFound.innerHTML = '';
+    totalItem.innerHTML = '';
     parentDiv.textContent = '';
     // Get Api 
     fetch(`https://openlibrary.org/search.json?q=${inptuValue}`)
@@ -36,12 +36,12 @@ const displaySearchResult = books => {
         return;
     }
     // Create dynamic div for count total search item 
-    const gotItemNumber = document.createElement('div')
-    gotItemNumber.innerHTML = `<h6 class= "text-dark text-center">Number of Books Found : ${books.numFound}</h5>`
-    itemFound.appendChild(gotItemNumber);
+    const getItemNumber = document.createElement('div')
+    getItemNumber.innerHTML = `<h6 class= "text-dark text-center">Number of Books Found : ${books.numFound}</h5>`
+    totalItem.appendChild(getItemNumber);
     // Get first 30 books details to display website 
     books.docs.slice(0, 29).forEach((book) => {
-        // undefined error handle 
+        // Undefined error handle 
         const imgId = book.cover_i === undefined ? '8236407' : book.cover_i;
         const author = book.author_name === undefined ? 'Not Found' : book.author_name;
         const publishedDate = book.first_publish_year === undefined ? 'Not Found' : book.first_publish_year;
@@ -49,12 +49,13 @@ const displaySearchResult = books => {
         // Push dynamic HTML 
         const div = document.createElement('div');
         div.innerHTML = `
-            <div class="card m-3 shadow-lg p-2" style="width: 18rem;">
+            <div class = "border border-success dynamic-result card m-1 my-4 shadow-lg p-2"
+            style = "width: 18rem;" >
                 <img src="https://covers.openlibrary.org/b/id/${imgId}-M.jpg" class="card-img-top img-fluid" alt="...">
                 <div class="card-body text-black">
-                    <h5 class="card-title">Book Name: ${book.title}</h5>
+                    <h5 class="card-title">Book's Name: ${book.title}</h5>
                     <p class="card-text m-1"><b>Author-Name:</b> ${author}</p>
-                    <p class="card-text m-1"><b>1st Published:</b> ${publishedDate}</p>
+                    <p class="card-text m-1"><b>1st Published Year:</b> ${publishedDate}</p>
                     <p class="card-text m-1"><b>Publisher:</b> ${publisher}</p>
 
                 </div>
